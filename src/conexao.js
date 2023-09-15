efd70fb7d4007f30d1d8c5f823a04ce9b4961be0
Francisco Walter <franciscowalterd@gmail.com>
2023-09-15 01:55:27 -0300

    adicionando conexao com database

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-	host: 'localhost',
+	host: 'ec2-44-213-228-107.compute-1.amazonaws.com',
 	port: 5432,
-	user: 'postgres',
-	password: 'postgres',
-	database: 'aula_autenticacao_criptografia',
+	user: 'dztfcqgcjlbvkf',
+	password: '2f2b0e31c8a54693fc0e60dbdcf903de9a6184398d91dcaaa281a2bcbba5fa89',
+	database: 'd27pn301fddtsj',
 })
 
 module.exports = pool
